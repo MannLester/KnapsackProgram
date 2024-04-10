18ae172faf2e873061fa3f4263aa747e1e708a8e
MannLester <127029268+MannLester@users.noreply.github.com>
2024-04-10 11:09:05 -0700

    Add files via upload

--- Knapsack.c
+++ Knapsack.c
@@ -9,7 +9,27 @@ double* calculate_ratios(int *weights, int *values, int num) {
     return ratios;
 }
 
-void selection_sort(double *arr, int *sorted_index, int num) {
+void selection_sort_value(double *arr, int *sorted_index, int num) {
+    for (int i = 0; i < num - 1; i++) {
+        int max = i;
+        for (int j = i + 1; j < num; j++) {
+            if (arr[j] > arr[max]) {
+                max = j;
+            }
+        }
+        // Swap ratio values
+        double temp = arr[max];
+        arr[max] = arr[i];
+        arr[i] = temp;
+
+        // Swap corresponding indices in sorted_index array
+        int temp_index = sorted_index[max];
+        sorted_index[max] = sorted_index[i];
+        sorted_index[i] = temp_index;
+    }
+}
+
+void selection_sort_weight(int *arr, int *sorted_index, int num) {
     for (int i = 0; i < num - 1; i++) {
         int max = i;
         for (int j = i + 1; j < num; j++) {
@@ -30,7 +50,7 @@ void selection_sort(double *arr, int *sorted_index, int num) {
 }
 
 void max_value(int *weights, int *values, int num, int capacity) {
-   
+    double total_value = 0;
     double *ratios = calculate_ratios(weights, values, num);
 
     int sorted_index[num];
@@ -38,22 +58,21 @@ void max_value(int *weights, int *values, int num, int capacity) {
         sorted_index[i] = i;
     }
 
-    selection_sort(ratios, sorted_index, num);
+    selection_sort_value(ratios, sorted_index, num);
 
     int fill = 0;
     double overall_value = 0;
 
     printf("The used items are: \n");
     for (int i = 0; i < num; i++) {
-        double total_value = 0;
         if (fill + weights[sorted_index[i]] <= capacity) {
-            total_value += values[sorted_index[i]];
+            total_value = values[sorted_index[i]];
             fill += weights[sorted_index[i]];
         } else if (fill + weights[sorted_index[i]] > capacity) {
-            total_value += ratios[sorted_index[i]] * (capacity - fill);
+            total_value = ratios[sorted_index[i]] * (capacity - fill);
             printf("Remaining Capacity filled with Item %d\n", sorted_index[i] + 1);
             fill = capacity;
-        } else {
+        } else{
             printf("Capacity full\n");
             break;
         }
@@ -76,26 +95,25 @@ void max_weight(int *weights, int *values, int num, int capacity) {
         sorted_index[i] = i;
     }
 	
-	selection_sort(sorted_weights, sorted_index, num);
-
+	selection_sort_weight(sorted_weights, sorted_index, num);
     // Fill the capacity using the highest weight items first
     int fill = 0;
     double overall_value = 0;
     printf("The used items are: \n");
     for (int i = 0; i < num; i++) {
         double total_value = 0;
-        if (fill + sorted_weights[i] <= capacity) {
+        if (fill + sorted_weights[i] < capacity) {
             total_value = sorted_weights[i] * (double)sorted_values[i] / weights[i];
             fill += sorted_weights[i];
         } else if (fill + sorted_weights[i] > capacity) {
             total_value = (capacity - fill) * (double)sorted_values[i] / weights[i];
-            printf("Remaining Capacity filled with Item %d\n", i + 1);
+            printf("Remaining Capacity filled with Item %d\n", sorted_index[i] + 1);
             fill = capacity;
-        } else {
+        } else  if (fill + weights[sorted_index[i]] == capacity){
             printf("Capacity full\n");
             break;
         }
-        printf("Item %d: Weight: %d, Value: %.2f\n", i + 1, sorted_weights[i], total_value);
+        printf("Item %d: Weight: %d, Value: %.2f\n", sorted_index[i] + 1, sorted_weights[i], total_value);
         overall_value += total_value;
     }
     printf("Overall Value is: %.2f\n", overall_value);
